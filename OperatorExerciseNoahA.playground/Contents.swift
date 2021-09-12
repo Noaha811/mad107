209ca7aa8764a88c13d5fbc2600c181fd113d253
Noaha811 <77990922+Noaha811@users.noreply.github.com>
2021-09-12 08:53:23 -0500

    Add files via upload

---
+++ OperatorExerciseNoahA.playground/Contents.swift
@@ -0,0 +1,34 @@
+//Noah Anderson
+
+//declaring and assigning values to each variable
+var addvar1 = 10
+var addvar2 = 20
+
+var subvar1 = 100
+var subvar2 = 50
+
+var multvar1 = 7
+var multvar2 = 11
+
+var divvar1 = 88
+var divvar2 = 11
+
+var pvar1 = 35
+var pvar2 = 8
+
+//Performing each operation with the correct variable, and assigning
+//it to a new variable
+
+var addresult = addvar1 + addvar2
+var subresult = subvar1 - subvar2
+var multresult = multvar1 * multvar2
+var divresult = divvar1 / divvar2
+var presult = pvar1 % pvar2
+
+// Prints the results of each statement to the console
+
+print("\(addvar1) + \(addvar2) = \(addresult)")
+print("\(subvar1) - \(subvar2) = \(subresult)")
+print("\(multvar1) * \(multvar2) = \(multresult)")
+print("\(divvar1) / \(multvar2) = \(divresult)")
+print("\(pvar1) % \(pvar2) = \(presult)")
